48a2c7802d3e08248fff3777e776985d27795e86
DENG JOSHUA <64380119+DengJoshua@users.noreply.github.com>
2020-12-09 10:56:48 +0300

    Version 4

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect, useState } from "react";
+
+import Screen from "./app/components/Screen";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
   return (
-  <ListingEditScreen />
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+    </Screen>
   );
 }
